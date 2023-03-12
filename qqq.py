0012a8f5810e6d5e1d96eec32e4d36d450c869f1
InJoy <www@qqq.com>
2023-03-12 23:14:57 +0300

    toncoin to usd

--- qqq.py
+++ qqq.py
@@ -1,5 +1,5 @@
 
 import requests
 
-res = requests.get('https://api.coingecko.com/api/v3/ping').json()
+res = requests.get(  'https://api.coingecko.com/api/v3/simple/price?ids=the-open-network&vs_currencies=usd').json()
 print(res)
